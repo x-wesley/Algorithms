94e57b72c4a3c81d4ed06e454ce0b4d720d2bacc
x-wesley <x-wesley@163.com>
2014-08-30 10:23:18 +0800

    剑指offer 面试题4 替换空格 题目：请实现一个函数，把字符串中的每个空格替换成“%20”。例如输入“We are happy.”， 则输出“We%20are%happy.”。

---
+++ src/com/auditionoffer/audition04/Replace.java
@@ -0,0 +1,14 @@
+package com.auditionoffer.audition04;
+
+public class Replace {
+	public Replace() {
+		// TODO Auto-generated constructor stub
+	}
+	public String replace(String str){
+		return str.replace(" ", "%20");
+	}
+	public String replace(char[] str){
+		
+		return "";
+	}
+}
---
+++ src/com/auditionoffer/audition04/Test.java
@@ -0,0 +1,11 @@
+package com.auditionoffer.audition04;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Replace r = new Replace();
+		System.out.println(r.replace("We are happy."));
+	}
+
+}
